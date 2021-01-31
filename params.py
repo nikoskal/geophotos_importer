47e12f496f5bd8331d19ce6a534b262447f8bdfc
nikosk <nikosk@cn.ntua.gr>
2021-01-31 18:04:39 +0200

    initial commit

---
+++ params.py
@@ -0,0 +1,21 @@
+t_host    = "localhost"
+t_port    = "5432"
+t_dbname  = "insert_db_name"
+t_user    = "insert_user"
+t_pw      = "insert_pw"
+
+
+photo_request_data = {
+    "CAD6E": {
+        "id": 218,
+        "coords_x": -5.9357153,
+        "coords_y": 54.5974748,
+        "land_hash": "1530850C9A6F5FF56B66AC16301584EC"
+        },
+    "TEST1": {
+        "id": 1,
+        "coords_x": 11.111111,
+        "coords_y": 22.222222,
+        "land_hash": "11111111111111111111111"
+        }
+    }
